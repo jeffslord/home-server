67a24c12ef1b41f299ced4b93b6d96cf59eede2f
jeffslord <jeffscottlord@gmail.com>
2026-01-02 00:45:47 +0000

    Refactor Docker Compose configurations for various services, including updates to environment variables, volume paths, and added restart policies for improved stability and organization.

---
+++ games/minecraft/cobbleverse/restart.sh
@@ -0,0 +1,9 @@
+docker exec mc-server rcon-cli /say SERVER RESTART IN 5 MINUTES
+sleep 3m
+docker exec mc-server rcon-cli /say SERVER RESTART IN 2 MINUTES
+sleep 1m
+docker exec mc-server rcon-cli /say SERVER RESTART IN 1 MINUTES
+sleep 1m
+docker exec mc-server rcon-cli /say SERVER RESTARTING
+sleep 5s
+docker compose -f /home/jeff/git/home-server/games/minecraft/cobbleverse/docker-compose.yml restart
